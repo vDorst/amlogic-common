96ef5188aff5183f4ff08fce1715268600d22aed
Re van Dorst <opensource@vdorst.com>
2013-03-27 13:23:07 +0100

    M3: Fix compile error: 'IO_PERIPH_BASE' undeclared.

--- arch/arm/mach-meson3/include/mach/io.h
+++ arch/arm/mach-meson3/include/mach/io.h
@@ -17,6 +17,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
+#include <mach/am_regs.h>
+
 #ifndef __ASM_ARCH_IO_H
 #define __ASM_ARCH_IO_H
 
